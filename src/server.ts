d1392547a5f27b9f26bcca30e0ed94dae1fd6919
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-11-22 01:43:37 +0600

    initial project setup done

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from "./app";
+import mongoose from "mongoose";
+import config from "./config";
+
+async function server() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+server();
